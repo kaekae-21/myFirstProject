f8db85d5a2da71502c5bf452e4eb6b120c275057
kaekae-21 <64093823+kaekae-21@users.noreply.github.com>
2020-04-30 15:25:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function myfun()
+{
+    let x1= document.getElementById('text1').value;
+    let x2= document.getElementById('text2').value;
+    let x3= document.getElementById('text3').value;
+   alert("NAME: "+x1+"\n AGE: "+x2+"\nGENDER: "+x3);
+}
